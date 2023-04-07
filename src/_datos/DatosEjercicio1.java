e9a70fbf0ca67069d6d4ec77b98c827ce74cf8aa
jesus <jesus@jesus-PROX15>
2023-04-07 17:36:55 +0200

    Añadidos los ficheros de ejercicio y adecentado el datosEjercicio1

--- src/_datos/DatosEjercicio1.java
+++ src/_datos/DatosEjercicio1.java
@@ -4,25 +4,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import _datos.DatosEjercicio1.tipoCafe;
+import _datos.DatosEjercicio1.Cafe;
 import us.lsi.common.Files2;
 
 public class DatosEjercicio1 {
 // un tipo  variedad cafe y otro tipo cafe
-	public static record tipoCafe(Integer id, String nombre, Integer peso) {
-		//realmente el atributo id sobra 
-		// ya que vale lo mismo que la posición en la lista
-		public static tipoCafe create(String linea, Integer id) {
+	public static record Cafe( String nombre, Integer peso) {
+
+		public static Cafe create(String linea) {
 		String[] datos = linea.split(":");//C01: kgdisponibles=35;
 		String nom = datos[0].trim();
 		String aux = datos[1].trim().replace("kgdisponibles=", " ").replace(';', ' ').trim();
 		Integer peso = Integer.valueOf(aux);
-		return new tipoCafe(id, nom, peso);
+		return new Cafe(nom, peso);
 		}
 	}
 
-	public static record variedadCafe(Integer id, String nombre, Double beneficio, Map<String, Double> composicion) {
-		public static variedadCafe create( String linea ,Integer id) {
+	public static record VarCafe( String nombre, Double beneficio, Map<String, Double> composicion) {
+		public static VarCafe create( String linea ) {
 			//P01 -> beneficio=60; comp=(C01:0.5),(C03:0.4),(C07:0.1);
 			String[] partes = linea.split(";");
 			//nomYBen  P01 -> beneficio=60
@@ -40,14 +39,13 @@ public class DatosEjercicio1 {
 			 Double valor = Double.valueOf(claveYvalor[1].trim());
 			comp.put(clave, valor);
 			}
-			return new variedadCafe(id, nom, ben, comp);
+			return new VarCafe( nom, ben, comp);
 		}
 	}
-	private static List<tipoCafe> cafe;
-	private static List<variedadCafe> variedades;
+	private static List<Cafe> cafe;
+	private static List<VarCafe> variedades;
 	public static void iniDatos(String fichero) {
-		Integer contCaf = -1;
-		Integer contVar = -1;
+		
 		List<String> ls =  Files2.linesFromFile(fichero);
 		
 		//el index de café lo podemos omitir ya que empieza siempre en cero
@@ -55,12 +53,12 @@ public class DatosEjercicio1 {
 		Integer fin = ls.size();
 		List<String> lsCafe = List.copyOf(ls.subList(1, indexVariedades)); 
 		List<String> lsVariedad = List.copyOf(ls.subList(indexVariedades+1, fin));
-		cafe = lsCafe.stream().map(s-> tipoCafe.create(s, contCaf+1)).toList();
-		variedades = lsVariedad.stream().map(s-> variedadCafe.create(s,contVar+1)).toList();
+		cafe = lsCafe.stream().map(s-> Cafe.create(s)).toList();
+		variedades = lsVariedad.stream().map(s-> VarCafe.create(s)).toList();
 		
 	}
 
-public static tipoCafe getCafeNom(String nom) {
+public static Cafe getCafeNom(String nom) {
 	return cafe.stream().filter(c->c.nombre().equals(nom)).findFirst().get();
 }
 	public static String cafeId(Integer id) {
@@ -82,11 +80,11 @@ public static tipoCafe getCafeNom(String nom) {
 	}
 
 	// cantidad
-	public static List<tipoCafe> getCafes() {
+	public static List<Cafe> getCafes() {
 		return cafe;
 	}
 
-	public static List<variedadCafe> getVariedades() {
+	public static List<VarCafe> getVariedades() {
 		return variedades;
 	}
 private static void testLectura() {

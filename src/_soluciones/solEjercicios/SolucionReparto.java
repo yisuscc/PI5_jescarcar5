73e8b91c06985063df7d1414c09ca4d6179f015e
yisus <yisus@yisus-PROX15>
2023-05-07 20:39:19 +0200

    la versión automatica del ejercicio 4 funciona, solo queda la manual y la heuristica

--- src/_soluciones/solEjercicios/SolucionReparto.java
+++ src/_soluciones/solEjercicios/SolucionReparto.java
@@ -16,6 +16,7 @@ public class SolucionReparto implements Comparable<SolucionReparto> {
 	List<Integer> solucion;
 	List<Integer> path;
 	Double beneficio;
+	Double kms;
 	public static SolucionReparto of(List<Integer> ls) {
 		return new SolucionReparto(ls);
 	}
@@ -27,11 +28,34 @@ public class SolucionReparto implements Comparable<SolucionReparto> {
 	}
 	private SolucionReparto(List<Integer>values) {
 		//TODO REHACER 
+		//Double ben = values.stream().mapToDouble(i-> DatosEjercicio4.getBeneficioCliente(i)).sum();
+		//values = [x,x,x,x,x,x]
+		List<Integer>v = List2.addFirst(values,0);
+		v = List2.addLast(v, 0);
+		// v = [0,x,x,x,x,x,x,0]
+		Double kmetros = 0.;
+		Double ben = 0.;
+		Double k= 0.;
+		for(Integer i = 0;i<v.size(); i++ ) {
+		Integer j = i+1;
+		if(j== v.size())
+			break;
+		Integer v1 = v.get(i);
+		Integer v2 = v.get(j);
+		k = DatosEjercicio4.getPesoArista(v1, v2);
+		kmetros += DatosEjercicio4.getPesoArista(v1, v2);
+		ben+= DatosEjercicio4.getBeneficioCliente(v2)-kmetros;
+		} //si la distancia es incorrecta comentad la linea siguiente
+		kmetros-= k;
+		kms= kmetros;
+		beneficio = ben;
+		solucion = v;
+		
 		
 	}
 	@Override
 	public String toString() {
-		String res = "Ruta optima: " +solucion+"\r\n"+"Benenficio:"+beneficio;
+		String res = "Ruta optima: " +solucion+"\r\n"+"Beneficio:"+beneficio+"\r\n"+"Kms: "+kms+"\r\n"+"La distancia en kilometros puede ser imprecisa(ver linea 48)";
 		
 		return res;
 	}
